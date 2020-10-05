50eff1709a75726eb744065b210a8b28ad33b129
Horea Oros <horea.oros@gmail.com>
2020-10-05 16:22:23 +0300

    adaugat cod pentru citire date de la consola

--- ASC1/Program.cs
+++ ASC1/Program.cs
@@ -11,6 +11,27 @@ namespace ASC1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word of C#!!");
+            Console.WriteLine("Introduceti numarul de pantof: ");
+            
+            string line;
+            line = Console.ReadLine();
+
+            int numarPantof;
+
+            try
+            {
+                numarPantof = int.Parse(line);
+                
+                Console.WriteLine($"Aveti {numarPantof} numarul la pantof");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            //int.TryParse(line, out numarPantof);
+
         }
     }
 }
