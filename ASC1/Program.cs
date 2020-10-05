dfe1172c44de1cefe0b6207707c8664c6ef70ecb
codemarked <bartharoland2001@gmail.com>
2020-10-05 16:45:14 +0300

    changes#1

--- ASC1/Program.cs
+++ ASC1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ASC1
 {
@@ -10,25 +6,17 @@ namespace ASC1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word of C#!!");
+            Console.WriteLine("Hello World of C#!!");
             Console.WriteLine("Introduceti numarul de pantof: ");
-            
-            string line;
-            line = Console.ReadLine();
 
             int numarPantof;
-
-            try
-            {
-                numarPantof = int.Parse(line);
-                
+            try {
+                numarPantof = int.Parse(Console.ReadLine());
                 Console.WriteLine($"Aveti {numarPantof} numarul la pantof");
+            } catch (Exception e) {
+                Console.WriteLine("Va rugam sa folositi doar numere intregi!");
+                return;
             }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
 
             //int.TryParse(line, out numarPantof);
 
